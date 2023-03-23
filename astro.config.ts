7bf735136e3d19c1ef730cd2deee2d53e594db30
Ryan Wang <i@ryanc.cc>
2023-03-23 11:18:36 +0800

    feat: add tag and category page

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), vue()],
   server: {
     port: 4000,
-  },
+  }
 });
